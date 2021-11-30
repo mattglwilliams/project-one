a4abea414fbe97ed93205e5a351b4acbf6bb8f7b
Matthew Williams <matthewwilliams@matts-mbp.home>
2021-11-30 22:21:26 +0000

    Updated script

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var fetchMovieData = function () {
-    var currentApiURL = "https://api.themoviedb.org/3/discover/movie?api_key=2fad5e038a5d373957de8c81a2825905&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=100&with_genres=comedy&genres=action&with_watch_monetization_types=flatrate";
+// var fetchMovieData = function () {
+//     var currentApiURL = "https://api.themoviedb.org/3/discover/movie?api_key=2fad5e038a5d373957de8c81a2825905&language=en-UK&network_name=netflix&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=comedy&genres=action&with_watch_monetization_types=flatrate&with_original_language=en";
 
-    fetch(currentApiURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-}
+//     fetch(currentApiURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+// }
 
-fetchMovieData()
+// fetchMovieData()
 
 var fetchTVData = function () {
-    var currentApiURL = "https://api.themoviedb.org/3/discover/tv?api_key=2fad5e038a5d373957de8c81a2825905&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=500&with_genres=comedy&with_watch_monetization_types=flatrate";
+    var currentApiURL = "https://api.themoviedb.org/3/discover/tv?api_key=2fad5e038a5d373957de8c81a2825905&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_networks=netflix&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0";
 
     fetch(currentApiURL)
         .then(function (response) {
@@ -25,3 +25,17 @@ var fetchTVData = function () {
 }
 
 fetchTVData();
+
+// var fetchNetwork = function () {
+//     var currentApiURL = "https://api.themoviedb.org/3/network/49?api_key=2fad5e038a5d373957de8c81a2825905";
+
+//     fetch(currentApiURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+// }
+
+// fetchNetwork();
