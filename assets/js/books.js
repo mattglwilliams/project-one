de4ff584374a25e21097a74c3c26952f260206f1
Matt <86723332+mattmc86@users.noreply.github.com>
2021-12-04 10:39:02 +0000

    Added functionality to generate a random page and loop through first 3 results

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,86 +1,338 @@
-var bookCardEl = document.querySelector('.bookCard');
-var bookButtonEl=document.querySelector('.bookButton');
-var searchAgainEl=document.querySelector('.searchAgain');
+//variables for elements to render
 var displayBookEl = document.querySelector('.displayBook');
 
-function getBook() {
+// variables for buttons 
+var actionBookBtn = document.querySelector(".action")
+var adventureBookBtn = document.querySelector(".adventure")
+var comedyBookBtn = document.querySelector(".comedy")
+var crimeBookBtn = document.querySelector(".crime")
+var fantasyBookBtn = document.querySelector(".fantasy")
+var historyBookBtn = document.querySelector(".history")
+var horrorBookBtn = document.querySelector(".horror")
+var kidsBookBtn = document.querySelector(".kids")
+var musicBookBtn = document.querySelector(".music")
+var mysteryBookBtn = document.querySelector(".mystery")
+var nonFictionBookBtn = document.querySelector(".non-fiction")
+var politicsBookBtn = document.querySelector(".politics")
+var romanceBookBtn = document.querySelector(".romance")
+var sportBookBtn = document.querySelector(".sport")
+var thrillerBookBtn = document.querySelector(".thriller")
+var warBookBtn = document.querySelector(".war")
+var westernBookBtn = document.querySelector(".western")
+
+// variables to get value from buttons
+var actionBookValue = actionBookBtn.value
+var adventureBookValue = adventureBookBtn.value
+var comedyBookValue = comedyBookBtn.value
+var crimeBookValue = crimeBookBtn.value
+var fantasyBookValue = fantasyBookBtn.value
+var historyBookValue = historyBookBtn.value
+var horrorBookValue = horrorBookBtn.value
+var kidsBookValue = kidsBookBtn.value
+var musicBookValue = musicBookBtn.value
+var mysteryBookValue = mysteryBookBtn.value
+var nonFictionBookValue = nonFictionBookBtn.value
+var politicsBookValue = politicsBookBtn.value
+var romanceBookValue = romanceBookBtn.value
+var sportBookValue = sportBookBtn.value
+var thrillerBookValue = thrillerBookBtn.value
+var warBookValue = warBookBtn.value
+var westernBookValue = westernBookBtn.value
+
+// to use in URL so a random page is returned from API
+var randomNum = Math.floor(Math.random() * 10)
+
+//functions for each genre button to return data from API
+var fetchActionBookData = function () {
+
+    var actionBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +actionBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(actionBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+actionBookBtn.addEventListener("click", fetchActionBookData)
+
+var fetchAdventureBookData = function () {
+
+    var adventureBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +adventureBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(adventureBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+adventureBookBtn.addEventListener("click", fetchAdventureBookData)
 
 
-var genreEl = document.querySelector('.genre');
-var genreEl2 = document.querySelector('.genre2');
-var genreEl3 = document.querySelector('.genre3');
-var genreName = genreEl.value.trim();
-var genreName2 = genreEl2.value.trim();
-var genreName3 = genreEl3.value.trim();
-var randomNum = Math.floor(Math.random() * 20);
+var fetchComedyBookData = function () {
+
+    var comedyBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +comedyBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(comedyBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+comedyBookBtn.addEventListener("click", fetchComedyBookData)
+
+var fetchCrimeBookData = function () {
+
+    var crimeBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +crimeBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(crimeBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+crimeBookBtn.addEventListener("click", fetchCrimeBookData)
 
 
-var genreUrl = ['https://www.googleapis.com/books/v1/volumes?q=subject:' +genreName+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU','https://www.googleapis.com/books/v1/volumes?q=subject:' +genreName2+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU','https://www.googleapis.com/books/v1/volumes?q=subject:' +genreName3+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU'];
-  for (var i=0;i<=genreUrl.length;i++){
-    fetch(genreUrl[i])
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+var fantasyBookData = function () {
 
-      console.log(data);
-      for (var i = 0; i<=2;i++){
-        var bookTitle = document.createElement('p');
-        var bookImage =document.createElement('img');
-        displayBookEl.appendChild(bookTitle);
+    var fantasyBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +fantasyBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
 
-        bookTitle.textContent= `Title: ${data.items[i].volumeInfo.title}`;
-        
-        bookImage.setAttribute("src", data.items[i].volumeInfo.imageLinks.smallThumbnail);
-          if (data.items[i].volumeInfo.imageLinks.smallThumbnail){
-            
-            displayBookEl.appendChild(bookImage);
-          }else{
-            bookImage.setAttribute("src", noImage.webp)
-          }
-        }
-     
+    fetch(fantasyBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
-      }
-      
-    );
+fantasyBookBtn.addEventListener("click", fantasyBookData)
+
+var historyBookData = function () {
+
+    var historyBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + historyBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(historyBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+historyBookBtn.addEventListener("click", historyBookData)
+
+var horrorBookData = function () {
+
+    var horrorBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +horrorBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+    fetch(horrorBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+horrorBookBtn.addEventListener("click", horrorBookData)
+
+var kidsBookData = function () {
+
+  var kidsBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +kidsBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+  fetch(kidsBookApiURL)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          console.log(data);
+      })
+}
+
+kidsBookBtn.addEventListener("click", kidsBookData)
 
 
-  }
-   
-  
-};
 
-// var genreUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:' +genreName+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
-//   fetch(genreUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
+var musicBookData = function () {
 
-//       console.log(data);
-//       for (var i = 0; i<=5;i++){
-//         var bookTitle = document.createElement('p');
-//         var bookImage =document.createElement('img');
-//         displayBookEl.appendChild(bookTitle);
+    var musicBookApiURL = 'https:www.googleapis.com/books/v1/volumes?q=subject:' +musicBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
 
-//         bookTitle.textContent= `Title: ${data.items[i].volumeInfo.title}`;
-        
-//         bookImage.setAttribute("src", data.items[i].volumeInfo.imageLinks.smallThumbnail);
-//           if (data.items[i].volumeInfo.imageLinks.smallThumbnail){
-            
-//             displayBookEl.appendChild(bookImage);
-//           }else{
-//             bookImage.setAttribute("src", ball.jpg)
-//           }
-//         }
-     
+    fetch(musicBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
-//       }
-      
-//     );
-  
-// };
+musicBookBtn.addEventListener("click", musicBookData)
 
-bookButtonEl.addEventListener('click',getBook);
+var mysteryBookData = function () {
 
+    var mysteryBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +mysteryBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(mysteryBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+mysteryBookBtn.addEventListener("click", mysteryBookData)
+
+var nonFictionBookData = function () {
+
+  var nonFictionBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" +nonFictionBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+  fetch(nonFictionBookApiURL)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          console.log(data);
+      })
+}
+
+nonFictionBookBtn.addEventListener("click", nonFictionBookData)
+
+var politicsBookData = function () {
+
+  var politicsBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + politicsBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+  fetch(politicsBookApiURL)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          console.log(data);
+      })
+}
+
+politicsBookBtn.addEventListener("click", politicsBookData)
+
+var romanceBookData = function () {
+
+    var romanceBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + romanceBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(romanceBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+romanceBookBtn.addEventListener("click", romanceBookData)
+
+
+
+var sportBookData = function () {
+
+    var sportBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + sportBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(sportBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+sportBookBtn.addEventListener("click", sportBookData)
+
+var thrillerBookData = function () {
+
+    var thrillerBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + thrillerBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(thrillerBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+thrillerBookBtn.addEventListener("click", thrillerBookData)
+
+var warBookData = function () {
+
+    var warBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + warBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(warBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+warBookBtn.addEventListener("click", warBookData)
+
+
+
+var westernBookData = function () {
+
+    var westernBookApiURL = "https://www.googleapis.com/books/v1/volumes?q=subject:" + westernBookValue+ '&orderBy=newest&startIndex='+randomNum+'&country:GB&language:en&key=AIzaSyA_lIqrTexQ5k7tVp1cuAOHbptlInieZKU';
+
+    fetch(westernBookApiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i<=2;i++){
+                    var bookTitle = document.createElement('p');
+                    var bookImage =document.createElement('img');
+                    var bookDescription = document.createElement('p');
+                    bookTitle.textContent= `Title: ${data.items[i].volumeInfo.title}`;
+                    bookDescription.textContent = `Description: ${data.items[i].volumeInfo.description}`;
+                    console.log(bookTitle);
+                    console.log(bookDescription);
+                    localStorage.setItem('bookTitle', bookTitle);
+                    localStorage.setItem('bookDescription', bookDescription);
+                    document.location = './book-results.html';
+                    localStorage.getItem('bookTitle');
+                    localStorage.getItem('bookDescription');
+                    displayBookEl.append(bookTitle);
+                    displayBookEl.append(bookDescription);
+              
+                    bookImage.setAttribute("src", data.items[i].volumeInfo.imageLinks.smallThumbnail);
+                    if (data.items[i].volumeInfo.imageLinks.smallThumbnail){
+                          displayBookEl.append(bookImage);
+                    }else{
+                          bookImage.setAttribute("src", noImage.webp) //TODO - get a placeholder image for books with no thumbnail
+                    }
+            }
+           
+        })
+}
+
+westernBookBtn.addEventListener("click", westernBookData)
+
+
+
+// var scores = JSON.parse(localStorage.getItem("scoreTotal"))|| [];
+//   initials = initials.value;
+//   var newScore = { initials, score };
+//   console.log(newScore)
+//   scores.push(newScore);
+//   localStorage.setItem("scoreTotal", JSON.stringify(scores));
