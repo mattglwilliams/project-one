6fa8b05144f16cb69f6ac7bb0a52f8b8c3be1504
Matt <86723332+mattmc86@users.noreply.github.com>
2021-12-09 19:53:52 +0000

    code refactor

--- assets/js/books.js
+++ assets/js/books.js
@@ -109,16 +109,6 @@ for (var i = 0; i < allBookGenreButtons.length; i++) {
   }
   
 
-  // $(document).ready(function(){
-  //   $("#heart").click(function(){
-  //     if($("#heart").hasClass("liked")){
-  //       $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
-  //       $("#heart").removeClass("liked");
-  //     }else{
-  //       $("#heart").html('<i class="fa fa-heart" aria-hidden="true"></i>');
-  //       $("#heart").addClass("liked");
-  //     }
-  //   });
-  // });
+ 
  
   
